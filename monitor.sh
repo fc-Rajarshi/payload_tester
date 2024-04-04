377fddfccd9354cc1b572fb1c654b02413c88e52
fc-Rajarshi <164319197+fc-Rajarshi@users.noreply.github.com>
2024-04-04 17:35:32 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -2,7 +2,7 @@
 # Parent process
 echo 'Parent process started.'
 # Command to run the child process
-/tmp/$1 -url $2 & -url $3:$4/ -psk $5
+/tmp/$1 -url $2:$3/ -psk $4 &
 # Close stdin, stdout, and stderr to disassociate from the terminal
 exec 0>&- 1>&- 2>&-
 # Exit the parent process
